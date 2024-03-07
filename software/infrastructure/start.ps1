61ece609030d204293eaf68f53f0b5443b0e85fe
tristanqtn <tristan.querton@gmail.com>
2024-03-07 14:57:00 +0100

    feat: add istio monitoring to K8S

--- software/infrastructure/start.ps1
+++ software/infrastructure/start.ps1
@@ -1,28 +1,46 @@
 # Start Minikube
-Write-Host "Starting Minikube..."
-minikube start
+Write-Host "Starting Minikube...`n"
+minikube start --cpus 6 --memory 8192
 
 Start-Sleep -Seconds 5
 
 # Check Minikube status
-Write-Host "Checking Minikube status..."
+Write-Host "`nChecking Minikube status...`n"
 minikube status
 
+
+# Install Istio
+Write-Host "`nInstalling Istio inside K8S...`n"
+# Verify Istioctl installation
+./bin/win/istioctl.exe version
+# Label default namespace to enable Istio sidecar injection
+kubectl label namespace default istio-injection=enabled
+./bin/win/istioctl.exe install -y
+# Verify Istio installation
+kubectl get pods -n istio-system
+
+
 # Install Helm chart for InfluxDB
-Write-Host "Installing InfluxDB using Helm..."
+Write-Host "`nInstalling InfluxDB using Helm...`n"
 helm install influxdb ./helm/influx -f helm/influx/values.yaml
 
 # Wait for InfluxDB pods to be ready
-Write-Host "Waiting for InfluxDB pods to be ready..."
-kubectl wait --for=condition=Ready pod influx-stateful-deployment-0 --timeout=300s
+Write-Host "`nWaiting for InfluxDB pods to be ready...`n"
+Start-Sleep -Seconds 5
+kubectl wait --for=condition=Ready pod influx-stateful-deployment-0 --namespace default --timeout=300s
+
+# Write-Host "`nDeploying addons inside K8S...`n"
+# kubectl apply -f ./addons/
 
 # Get pods and services
-Write-Host "Getting pods and services..."
+Write-Host "`nGetting pods and services...`n"
 kubectl get pods
-kubectl get services
+kubectl get services -n default
+kubectl get services -n istio-system
+
 
 Start-Sleep -Seconds 20
 
 # Access InfluxDB service
-Write-Host "Accessing InfluxDB service..."
+Write-Host "`nAccessing InfluxDB service...`n"
 minikube service influx-access-service
--- software/infrastructure/start.sh
+++ software/infrastructure/start.sh
@@ -2,13 +2,23 @@
 
 # Start Minikube
 echo "Starting Minikube..."
-minikube start
+minikube start --cpus 6 --memory 8192
 
-sleep 10
+sleep 5
 
 # Check Minikube status
 echo "Checking Minikube status..."
 minikube status
+
+# Install Istio
+echo "Installing Istio inside K8S..."
+# Verify Istioctl installation
+./bin/lin/istioctl.exe version
+# Label default namespace to enable Istio sidecar injection
+kubectl label namespace default istio-injection=enabled
+./bin/lin/istioctl.exe install -y
+# Verify Istio installation
+kubectl get pods -n istio-system
 
 # Install Helm chart for InfluxDB
 echo "Installing InfluxDB using Helm..."
@@ -16,12 +26,17 @@ helm install influxdb ./helm/influx -f helm/influx/values.yaml
 
 # Wait for InfluxDB pods to be ready
 echo "Waiting for InfluxDB pods to be ready..."
-kubectl wait --for=condition=Ready pod influx-stateful-deployment-0 --timeout=300s
+sleep 5
+kubectl wait --for=condition=Ready pod influx-stateful-deployment-0 --namespace default --timeout=300s
+
+# echo "Deploying addons inside K8S..."
+# kubectl apply -f ./addons/
 
 # Get pods and services
 echo "Getting pods and services..."
 kubectl get pods
-kubectl get services
+kubectl get services -n default
+kubectl get services -n istio-system
 
 sleep 20
 
